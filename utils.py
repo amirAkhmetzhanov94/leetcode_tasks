a7d47107d45887b7d448842bd0557c8a1429e6fd
Амир Ахметжанов <amirah@halykbank.kz>
2024-10-26 10:07:29 +0500

    bruteforce solved 21 + utils for linked lists

--- utils.py
+++ utils.py
@@ -6,4 +6,28 @@ def add_underscores_to_title(title: str) -> str:
     )
 
 
-print(add_underscores_to_title(''))
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def transform_lists_into_linked(non_linked_list: list):
+    if not non_linked_list:
+        return
+
+    head = ListNode(non_linked_list[0])
+    current = head
+
+    for number in non_linked_list[1:]:
+        current.next = ListNode(number)
+        current = current.next
+
+    return head
+
+
+def iterate_over_linked_list(linked_list):
+    current = linked_list
+    while current:
+        print(current.val)
+        current = current.next
